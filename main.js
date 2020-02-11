0ee508f56f5c97d3cbbe4afdf808cb654962a79d
Mohit Suman <mohit.suman@booking.com>
2020-02-11 16:02:08 +0100

    Adding rest of the things that are there in ES6 so far.

--- main.js
+++ main.js
@@ -448,7 +448,7 @@ class Feline extends Animal{
 		this.domestic = domestic;
 	}
 
-	doTheCatLoveYou(didYouGiveFood) {
+	doesTheCatLoveYou(didYouGiveFood) {
 		super.eats("Rubbish");
 		return didYouGiveFood && this.domestic;
 	}
@@ -479,10 +479,431 @@ var Feline = function(name, energy, domestic) {
 
 Feline.prototype = new Animal();
 
-Feline.prototype.doTheCatLoveYou = function(didYouGiveFood) {
+Feline.prototype.doesTheCatLoveYou = function(didYouGiveFood) {
 	this.eats('rubbish');
 	return didYouGiveFood && this.domestic;
 };
+
+
+/**
+ * Sets
+ */
+
+/**
+ * In ES6 there's a new built-in objects called Sets.
+ * Roughly speaking sets are like arrays but sets have the following props
+ * - The elements in sets are unique.
+ * - Sets are not index based so we can't refer to items based on their position.
+ * - Items in Set cannot be accessed individually.
+ */
+
+
+/**
+ * Following are examples of proper set creation
+ *
+ */
+
+let setA = new Set([1, 2, 3, 0]);
+let setB = new Set([1, false, 0, "0"]);
+let setC = new Set([1, 2, 3, false, false]); //It will only contain 4 values so the duplicate false will be ignored
+
+/**
+ * Modifying sets
+ */
+
+setA.delete(1);
+setA.add('Not a numberoo');
+
+
+// Adding a duplicate item in set will not result in error
+setA.add(1); // return the set itself
+
+// Removing a non-existent item form a set will also not result in a error
+setA.delete(25); // returns true or false
+
+// The size of a Set is accessed by .size instead of .length
+console.log(setA.length);  //undefined
+console.log(setA.size); // 5
+
+// Emptying a set
+setA.clear();
+
+// Checking whether an item exist in the set
+setB.has(1); // true
+
+
+let setBValues = setB.values();
+console.log(setBValues.next()); // {done: false, value: 1}
+
+/**
+ * A much easier way to do this is by using the for...of iterator
+ *
+ */
+
+for(const val of setB) {
+	console.log(val); // it will print value of elements in setB
+}
+
+/**
+ * WeakSet
+ */
+
+// A WeakSet only contains objects
+
+let suchAWeakSet = new WeakSet([{value: 23, name: 'Weakling'}]); // Good
+let suchAWrongWeakSet = new WeakSet([23]); //Error
+
+// A cool gotcha for WeakSet
+
+/**
+ * Consider the following example.
+ * We create an object and then add that object to
+ * a WeakSet
+ * Now when the object is deleted, the memory occupied by it will auto freed
+ * when the JS collection runs, you wont have to do anything special for this.
+ * So we don't have to worry about deleting the reference from the WeakSet
+ */
+
+// Important thing to not is that WeakSet don't have a .values function
+
+
+/***************************
+ * MAPS
+ ***************************/
+
+/**
+ * If sets are similar to Array then Maps are similar to Objects.
+ * Essentially maps let you store key value pairs where the both keys
+ * and values can be objects.
+ */
+
+
+// Creating a map
+const employees = new Map();
+
+employees.set('mohit.suman@gmail.com', {
+	firstName: 'Mohit',
+	lastName: 'Suman'
+});
+
+
+// To find out if a key exists
+employees.has('mohit.suman@gmail.com');
+
+// To get the value of a particular key
+let employee = employees.get('mohit.suman@gmail.com');
+
+// To delete an entry, simply use .delete
+employees.delete('mohit.suman@gmail.com');
+
+// To delete all entries from the set
+employees.clear();
+
+// To iterate over a map
+
+// You can use the keys
+let employeeValues = employees.values();
+employeeValues.next(); //Object {value : "mohit.suman@gmail.com", done: true}
+
+// You can also use the values
+let employeeKeys = employees.values();
+employeeKeys.next(); //Object {value: {firstName: "Mohit", lastName: "Suman"}, done: true }
+
+// Using a for...of loop
+// we get an array as the value which contains first the key then the value of the set
+
+for(const employee of employees) {
+	let [email, details] = employee;
+	console.log(email, details);
+}
+
+// Using a forEach loop
+employees.forEach((value, key) => console.log(value,key));
+
+/*************************
+ * WeakMaps
+ ************************/
+
+// We can only add objects as key of WeakMap
+// WeakMaps are not iterable
+// WeakMaps don't have a .clear method
+
+let book1 = {title: 'TLOR', author: 'Tera Baap'};
+let book2 = {title: 'TLOR 2', author: 'Tera Baap'};
+
+const library = new WeakMap();
+library.set(book1, true);
+library.set(book2, false);
+
+// If we add anything other than an object we will get an error
+library.set('Aloha', false);
+
+/** Again if set the reference of the book1 object
+ * to null then it will be deleted from the WeakMap as well
+ */
+
+book1 = null;
+
+console.log(library); // WeakMap {Object{title: 'TLOR 2', author: 'Tera Baap'} => false}
+
+/**
+ * Promises
+ */
+
+/**
+ * Promises basically take in a function as argument, which might complete in a bit
+ * It also passes two arguments to this function one called resolve and the other called reject
+ * When either resolve or reject is called, the promise is completed and any callbacks attached to
+ * the promise using the .then are called.
+ * Let's see how it works in practice
+ */
+
+
+let promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		if(Math.ceil(Math.random(1,10) * 10) % 2 === 0) {
+			resolve('Awesome');
+		} else {
+			reject('Not great');
+		}
+	}, 1000);
+});
+
+
+promise.then((data) => {
+	console.log(`The day is simply ${data}`);
+}, (data) => {
+	console.log(`It's a shame that the day is ${data}`);
+});
+
+
+/**
+ * Proxies
+ *
+ * Proxies basically acts as interceptors for objects
+ * So when the properties of an objects are accessed via a proxy
+ * The proxy can alter the behaviour, lets look at the example below
+ * to understand the concepts better.
+ */
+
+let object = {
+	name: "Mohit",
+	age: () => Math.ceil(Math.random(30, 50) * 10)
+};
+
+let objectHandler = {
+	get(target, propertyName) {
+		if(['name', 'age'].indexOf(propertyName) > -1) {
+			if(target.confidential) {
+				return `We cannot reveal the ${propertyName} to you`;
+			}
+		}
+		return target[propertyName];
+	},
+
+	set(target, propertyName, value) {
+		if(['name', 'age'].indexOf(propertyName) > -1) {
+			if(target.confidential) {
+				console.log('We cannot set the props for confidentiality reasons');
+				return;
+			}
+		}
+
+		target[propertyName] = value;
+	}
+};
+
+
+let objectProxy = new Proxy(object, objectHandler);
+
+// Here the name and age cannot be accessed
+// as the proxy as the object.confidential = true
+object.confidential = true;
+console.log(objectProxy.name);
+console.log(objectProxy.age());
+objectProxy.father = 'Sabka Maalik ek hai';
+
+// Here the name and age will be printed and will be accessible through
+// the proxy as the object.confidential = false
+object.confidential = false;
+console.log(objectProxy.name);
+console.log(objectProxy.age());
+
+
+/**
+ * Proxies vs ES5 getter and setter
+ */
+
+// In ES5 there were getter and setter
+
+var obj1 = {
+	_age: 34,
+	get age() {
+		console.log(this._age);
+		return this._age;
+	}
+};
+
+
+/** As you can see in the ES5 getter setter we couldn't work with
+ * dynamic name of the properties
+ */
+
+
+/*******************************
+ * Generators & Iterators
+ *
+ * It is one of the most confusing topic that I have witnessed so far in ES6.
+ * Basically we can pause a function execution for a bit, get a value.
+ * Then when we call the generator function again with .next() we resume the execution of the
+ * function, till we encounter the next "yield" statement
+ * What adds to the confusion is that you can pass value to the generator function as well.
+ *******************************/
+
+
+/**
+ * How to define a constructor function
+ * Having a asterisk between the function keyword and the name of the function will
+ * result in the function being a generator function
+ *
+ * So all of these are correct
+ *
+ *
+ * function *getStudent() {}
+ * function * getStudent() {}
+ * function* getStudent() {} - THIS IS THE STANDARD PRACTICE THOUGH
+ */
+
+function* getStudent() {
+	console.log('Func exec started');
+	const numbers = [1, 2, 3, 4 ,5, 6];
+
+	for (let num of numbers) {
+		console.log(num);
+	}
+
+	console.log('This is the last line of the function');
+}
+
+getStudent(); // Returns something like getStudent{<suspended>}
+
+
+/**
+ * IMPORTANT - When the a generator function runs it actually doesn't run
+ * any code inside the function. Instead it create and returns an iterator.
+ * The iterator can be used to execute the actual code inside the function
+ *
+ * So following up from our previous example:-
+ */
+
+let studentIterator = getStudent();
+studentIterator.next();
+/**
+ * Will print the all the six numbs
+ * 1
+ * 2
+ * 4
+ * 5
+ * 6
+ */
+
+/**
+ * Yield keyword
+ *
+ * yield keyword inside a generator function causes the function to stop
+ */
+
+function* getStudentWithYield() {
+	console.log('Func exec started');
+	const numbers = [1, 2, 3, 4 ,5, 6];
+
+	for (let num of numbers) {
+		console.log(num);
+		yield;
+	}
+
+	console.log('This is the last line of the function');
+}
+
+let studentIteratorYield = getStudentWithYield();
+studentIteratorYield.next();
+/**
+ * This will print 1 and stop executing.
+ * So would need to run the .next() 6 times for printing all values of the array
+ * And we would need to run it for the 7 time to print the last line of console.log();
+ */
+
+
+/**
+ * Return the a value from the generator function
+ */
+
+function* getStudentWithYieldReturn() {
+	console.log('Func exec started');
+	const numbers = [1, 2, 3, 4 ,5, 6];
+
+	for (let num of numbers) {
+		yield num;
+	}
+
+	console.log('This is the last line of the function');
+}
+
+let studentIteratorYieldReturn = getStudentWithYieldReturn();
+let firstNumber = studentIteratorYieldReturn.next();
+/**
+ * Basically yield is like a return you can say.
+ * firstNumber.value will contain the value 1, and firstNumber.done would be false
+ */
+
+
+/**
+ * Passing value in a generator
+ *
+ * Now this is the real tricky part of generator, and would require
+ * some iterations and practice from the brain to completely grasp the concept
+ * Let see how it works
+ */
+
+function* displayResponse() {
+	const response = yield;
+	console.log(`That's what you want me to print ${response}`);
+}
+
+let displayResponseIterator = displayResponse();
+displayResponseIterator.next();
+/**
+ * IMPORTANT First call to next needs to be run without passing any value
+ * This will just return without assigning any value
+ * When we run yield again then the value will be passed
+ */
+
+displayResponseIterator.next("Wassa Wassa Wassa"); // Will print the message and the function will run completely
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
